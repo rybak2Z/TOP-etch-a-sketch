3f546008af0e82fa3425c5978826873f6debf184
rybak2Z <rybak2Z.000@gmail.com>
2023-06-15 00:11:00 +0200

    Fill sketch with squares

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const SKETCH_SIZE = 10;
+const SQUARE_SIZE = 50; // in px
 
 const div = document.getElementById("sketch");
+const edgeLength = SQUARE_SIZE * SKETCH_SIZE;
+div.style.width = edgeLength + "px";
+div.style.height = edgeLength + "px";
 
 let square;
 let color;
-for (let i = 0; i < SKETCH_SIZE; i++) {
+for (let i = 0; i < SKETCH_SIZE * SKETCH_SIZE; i++) {
   square = document.createElement("div");
   square.classList.add("square");
   color = `hsl(${Math.random() * 360}, 100%, 50%)`;
