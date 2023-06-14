11fea7f4f898859eb727e8e27a9fed522c13d723
rybak2Z <rybak2Z.000@gmail.com>
2023-06-15 00:14:33 +0200

    Make squares white and board better visible

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ let color;
 for (let i = 0; i < SKETCH_SIZE * SKETCH_SIZE; i++) {
   square = document.createElement("div");
   square.classList.add("square");
-  color = `hsl(${Math.random() * 360}, 100%, 50%)`;
   square.style.backgroundColor = color;
   div.appendChild(square);
 }
