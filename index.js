45e569fbd3f53bebc4dab555d2eae2f41d83f01d
rybak2Z <rybak2Z.000@gmail.com>
2023-06-15 00:15:18 +0200

    Rename sketch to board

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const SKETCH_SIZE = 10;
 const SQUARE_SIZE = 50; // in px
 
-const div = document.getElementById("sketch");
+const board = document.getElementById("board");
 const edgeLength = SQUARE_SIZE * SKETCH_SIZE;
-div.style.width = edgeLength + "px";
-div.style.height = edgeLength + "px";
+board.style.width = edgeLength + "px";
+board.style.height = edgeLength + "px";
 
 let square;
 let color;
@@ -12,5 +12,5 @@ for (let i = 0; i < SKETCH_SIZE * SKETCH_SIZE; i++) {
   square = document.createElement("div");
   square.classList.add("square");
   square.style.backgroundColor = color;
-  div.appendChild(square);
+  board.appendChild(square);
 }
