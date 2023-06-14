7a3349119a5f82da110a9bab915fc0d558db41be
rybak2Z <rybak2Z.000@gmail.com>
2023-06-15 00:29:46 +0200

    Implement drawing

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ board.style.width = edgeLength + "px";
 board.style.height = edgeLength + "px";
 
 let square;
-let color;
+let inDrawMode = false;
 for (let i = 0; i < SKETCH_SIZE * SKETCH_SIZE; i++) {
   square = document.createElement("div");
   square.classList.add("square");
-  square.style.backgroundColor = color;
+
+  square.addEventListener("mousedown", (event) => {
+    inDrawMode = true;
+    event.target.style.backgroundColor = "black";
+  });
+  square.addEventListener("mouseenter", (event) => {
+    if (inDrawMode) {
+      event.target.style.backgroundColor = "black";
+    }
+  });
+
   board.appendChild(square);
 }
+
+document.addEventListener("mouseup", (event) => {
+  inDrawMode = false;
+});
