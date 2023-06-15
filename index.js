a711664eacdb1baf7e39c993b26e7d9c9b366432
rybak2Z <rybak2Z.000@gmail.com>
2023-06-15 12:38:23 +0200

    Make sketch size adjustable

--- index.js
+++ index.js
@@ -8,23 +8,50 @@ board.style.height = edgeLength + "px";
 
 let square;
 let inDrawMode = false;
-for (let i = 0; i < SKETCH_SIZE * SKETCH_SIZE; i++) {
-  square = document.createElement("div");
-  square.classList.add("square");
 
-  square.addEventListener("mousedown", (event) => {
-    inDrawMode = true;
-    event.target.style.backgroundColor = "black";
-  });
-  square.addEventListener("mouseenter", (event) => {
-    if (inDrawMode) {
+function createSquares(size) {
+  const squareSize = edgeLength / size;
+
+  for (let i = 0; i < size * size; i++) {
+    square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = squareSize + "px";
+    square.style.height = squareSize + "px";
+
+    square.addEventListener("mousedown", (event) => {
+      inDrawMode = true;
       event.target.style.backgroundColor = "black";
-    }
-  });
+    });
+    square.addEventListener("mouseenter", (event) => {
+      if (inDrawMode) {
+        event.target.style.backgroundColor = "black";
+      }
+    });
 
-  board.appendChild(square);
+    board.appendChild(square);
+  }
+}
+
+function deleteSquares() {
+  const squares = document.querySelectorAll(".square");
+  for (const square of squares) {
+    board.removeChild(square);
+  }
 }
 
 document.addEventListener("mouseup", (event) => {
   inDrawMode = false;
 });
+
+const sizeSlider = document.getElementById("sketch-size-slider");
+sizeSlider.addEventListener("change", (event) => {
+  deleteSquares();
+  createSquares(event.target.value);
+});
+const sketchSize = document.getElementById("sketch-size");
+sizeSlider.addEventListener("input", (event) => {
+  sketchSize.innerText = event.target.value;
+});
+sketchSize.innerText = sizeSlider.value;
+
+createSquares(sizeSlider.value);
